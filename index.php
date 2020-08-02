bceb42b2bf7ffb29b8187765f61c3a71781d63e8
Hendra Irawan <hendraulfil@gmail.com>
2020-08-02 09:04:33 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
   <h1>Hello World!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates at aliquid temporibus omnis nisi, assumenda alias ipsam nemo iusto laudantium? Expedita ex maiores magni assumenda itaque sapiente quod eius illo!</p>
 </body>
 </html>
